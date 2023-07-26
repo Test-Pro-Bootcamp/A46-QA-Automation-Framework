dd141b908ebe4f5513160db0e1120c8c123a9d24
Pol-Ol <140028393+Pol-Ol@users.noreply.github.com>
2023-07-26 17:27:17 -0400

    added new tests

---
+++ src/test/java/HomeWork16.java
@@ -0,0 +1,60 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomeWork16 extends BaseTest {
+    @Test
+    public void NavigationRegistrationTest() {
+
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//step1 Get URL
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+//step2 Click Registration Link
+       WebElement registrationLink = driver.findElement(By.id("hel"));
+       registrationLink.click();
+//assertion Compare expected and actual
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/registration.php");
+//step3 Email registration
+        WebElement email = driver.findElement(By.id("email"));
+        email.click();
+        email.clear();
+        email.sendKeys("valivkote+3@gmail.com");
+//step4 Click Registration Button
+        WebElement registrationButton = driver.findElement(By.id("button"));
+        registrationButton.click();
+//step5 Click login
+        WebElement login = driver.findElement(By.cssSelector("[class = 'button']"));
+        login.click();
+//step6 Click emailLogin
+        WebElement emailLogin = driver.findElement(By.cssSelector("[type ='email']"));
+        emailLogin.click();
+        emailLogin.clear();
+        emailLogin.sendKeys("valivkote+2@gmail.com");
+//step7 Click password
+        WebElement password = driver.findElement(By.cssSelector("[type = 'password']"));
+        password.click();
+        password.clear();
+        password.sendKeys("te$t$tudent");
+//step8 Click submit
+        WebElement submit = driver.findElement(By.cssSelector("[type = 'submit']"));
+        submit.click();
+//assertion Compare expected and actual
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Thread.sleep(3000);
+        WebElement avatar = driver.findElement(By.cssSelector("[class = 'avatar']"));
+        Assert.assertTrue(avatar.isDisplayed());
+          driver.quit();
+
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,35 +9,85 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+//    @Test
+//    public void LoginEmptyEmailPasswordTest() {
+////prereq
+////      Added ChromeOptions argument below to fix websocket error
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+////step1
+//        String url = "https://qa.koel.app/";
+//        driver.get(url);
+////        assertion
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+////        driver.quit();
+
     @Test
-    public void LoginEmptyEmailPasswordTest() {
+    public void ValidLoginTest() throws InterruptedException {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+//step1 Get URL
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+//step2 Enter email
+        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
+        email.click();
+        email.clear();
+        email.sendKeys("demo@class.com");
+//step3 Enter password
+        WebElement password = driver.findElement(By.cssSelector("[type='password']"));
+        password.click();
+        password.clear();
+        password.sendKeys("te$t$tudent");
+//step4 Click submit
+        WebElement submit = driver.findElement(By.cssSelector("[type='submit']"));
+        submit.click();
+//assertion Compare expected and actual
+      Assert.assertEquals(driver.getCurrentUrl(), url);
+//        Thread.sleep(3000);
+//        WebElement avatar = driver.findElement(By.cssSelector("[class = 'avatar']"));
+//        Assert.assertTrue(avatar.isDisplayed());
+//            driver.quit();
 
-        @Test
-        public void LoginEmptyEmailPasswordTest() {
+    }
 
+    @Test
+    public void InValidPasswordTest()  {
+// test passed with empty password, because assertion compares url
 //      Added ChromeOptions argument below to fix websocket error
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-            String url = "https://qa.koel.app/";
-            driver.get(url);
-            Assert.assertEquals(driver.getCurrentUrl(), url);
-            driver.quit();
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//step1 Get URL
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+//step2 Enter email
+        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
+        email.click();
+        email.clear();
+        email.sendKeys("demo@class.com");
+//step3 Enter password
+        WebElement password = driver.findElement(By.cssSelector("[type='password']"));
+        password.click();
+        password.clear();
+        password.sendKeys("");
+        //step4 Click submit
+        WebElement submit = driver.findElement(By.cssSelector("[type='submit']"));
+        submit.click();
+//assertion Compare expected and actual
+//        Thread.sleep(3000);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+//      WebElement avatar = driver.findElement(By.cssSelector("[class = 'avatar']"));
+//        Assert.assertTrue(avatar.isDisplayed());
+//don't useAssert.assertTrue(driver.getCurrentUrl().equals(url)); expected[true] but found [false]- it's not info
+//            driver.quit();
     }
 }
