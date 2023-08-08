927900ddced90d782ca35ea597ab5b02979ba80a
Daria Pavlyuk <daria.pavlyuk@testpro.io>
2023-08-07 22:48:51 -0400

    Homework24

--- src/test/java/pom/BaseTest.java
+++ src/test/java/pom/BaseTest.java
@@ -6,50 +6,91 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
-import pom.HomePage;
-import pom.LoginPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
     public static WebDriver driver;
-    public  WebDriverWait wait;
+    public WebDriverWait wait;
     public Actions actions;
     public String baseUrl;
-    protected static LoginPage loginPage ;
+    protected static LoginPage loginPage;
     protected static HomePage homePage;
-    @BeforeSuite
-    public void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
+    //@BeforeSuite
+    //public void setupClass() {
+    //   WebDriverManager.chromedriver().setup();
+    //}
 
     @BeforeMethod
     @Parameters({"baseUrl"})
-    public void setupBrowser(String baseUrl) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
+    public void setupBrowser(String baseUrl) throws MalformedURLException {
+        //      ChromeOptions options = new ChromeOptions();
+        //     options.addArguments("--remote-allow-origins=*");
+        //   options.addArguments("--disable-notifications");
+        //   options.addArguments("--start-maximized");
+        // driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //         baseUrl = "https://qa.koel.app/";
         driver.get(baseUrl);
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        actions = new Actions(driver) ;
+        actions = new Actions(driver);
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
 
     }
-    public void provideEmail (String email){
+
+    private WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.178:4444";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-firefox": // gradle clean test -Dbrowser=grid-firefox
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-safari": // gradle clean test -Dbrowser=grid-safari
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--start-maximized");
+                // options.addArguments("--headless=new");
+                return driver = new ChromeDriver(options);
+        }
+    }
+
+    public void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("[type ='email']"));
         emailField.click();
         emailField.sendKeys(email);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[type ='email']")));
     }
+
     //step3
     public void providePassword(String password) {
         WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
@@ -57,20 +98,21 @@ public class BaseTest {
         passwordField.sendKeys(password);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[type='password']")));
     }
+
     //step4
     public void clickSubmit() {
         WebElement submit = driver.findElement(By.cssSelector("[type ='submit']"));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[type ='submit']")));
         submit.click();
     }
-    public String getPlaylistName(){
-        WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)") ) ;
-        return playlistElement.getText() ;
+
+    public String getPlaylistName() {
+        WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        return playlistElement.getText();
     }
 
     @AfterMethod
-    public void tearDown() {
-        driver.quit();
+    public void tearDown() {driver.quit();
     }
 
     @DataProvider(name = "invalidLoginProviders")
---
+++ src/test/java/pom/Homework24.java
@@ -0,0 +1,25 @@
+package pom;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework24 extends BaseTest{
+    @Test
+    public void renamePlaylist(){
+        String playlistName = "Daria46";
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        loginPage.provideEmail("daria.pavlyuk@testpro.io")
+                .providePassword("te$t$tudent")
+                .clickSubmitBtn();
+        homePage.doubleClickFirstPlayList()
+                .enterPlaylistName(playlistName );
+
+        Assert.assertEquals(homePage.getPlaylistName(),playlistName );
+
+
+    }
+
+}
+
+
